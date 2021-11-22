5c52bdfcca01cb4c68202c1d5976e0af570571fb
Alejandro <alejo1ibarra@gmail.com>
2021-11-21 20:50:49 -0500

    initial auth

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
 # Path
 from database import Base, engine
-from routers import post, user
+from routers import post, user, authentication
 
 # FastAPI
 from fastapi import FastAPI
@@ -9,5 +9,6 @@ from fastapi import FastAPI
 Base.metadata.create_all(engine)
 app = FastAPI()
 
+app.include_router(authentication.router)
 app.include_router(post.router)
 app.include_router(user.router)
---
+++ blog/routers/authentication.py
@@ -0,0 +1,11 @@
+# FastAPI
+from fastapi import APIRouter
+
+router = APIRouter(
+    tags=['Authentication']
+)
+
+
+@router.post('/login')
+def login():
+    return 'login'
